c71e428d20ef86ae8669bf901a41e0311e3f8790
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-06-23 12:39:44 -0700

    Added the unique_list_id api attribute

--- spec/resources/list_spec.rb
+++ spec/resources/list_spec.rb
@@ -9,6 +9,7 @@ describe AWeber::Resources::List do
   it { should respond_to :campaigns_collection_link }
   it { should respond_to :http_etag }
   it { should respond_to :id }
+  it { should respond_to :unique_list_id }
   it { should respond_to :name }
   it { should respond_to :resource_type_link }
   it { should respond_to :self_link }
